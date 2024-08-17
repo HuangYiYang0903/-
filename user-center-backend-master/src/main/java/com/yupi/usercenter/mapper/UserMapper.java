e786782a6abf78cc0dc185bf6689f9f822b81b43
黄依炀 <2694385724@qq.com>
2024-08-17 21:44:26 +0800

    企业人员管理系统

---
+++ user-center-backend-master/src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.yupi.usercenter.mapper;
+
+import com.yupi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ * @author 黄依炀
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
